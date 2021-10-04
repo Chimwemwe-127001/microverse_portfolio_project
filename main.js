f549a17743183355beaf384e4a45aa95dbbc77bb
Chimwemwe-127001 <sinyinzachimwemwe@gmail.com>
2021-10-04 12:44:31 +0200

    Add more js events

--- main.js
+++ main.js
@@ -2,12 +2,22 @@ const toolbar = document.querySelector('.toolbar__2')
 const topCross = document.querySelector('.top')
 const middleCross = document.querySelector('.middle')
 const bottomCross = document.querySelector('.bottom')
+const headline = document.querySelector('.headline')
+const works = document.querySelector('.works');
+const About = document.querySelector('.about-me');
+const form = document.querySelector('.form')
+const footer = document.querySelector('.footer')
 
 function toggleNavShow(){
     toolbar.classList.toggle('show');
     topCross.classList.toggle('cross');
     middleCross.classList.toggle('cross');
     bottomCross.classList.toggle('disappear');
+    headline.classList.toggle('blur');
+    works.classList.toggle('blur');
+    About.classList.toggle('blur');
+    form.classList.toggle('blur');
+    footer.classList.toggle('blur');
 }
 
 document.getElementById('btn-trigger').onclick = function() {
