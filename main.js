a2caaf01701f5439390a544cb2acd87104af5798
Ricky Mormor <rickymormor1@gmail.com>
2021-10-04 13:24:50 +0000

    Fix linter issues

--- main.js
+++ main.js
@@ -1,29 +1,30 @@
-const toolbar = document.querySelector('.toolbar__2')
-const topCross = document.querySelector('.top')
-const middleCross = document.querySelector('.middle')
-const bottomCross = document.querySelector('.bottom')
-const headline = document.querySelector('.headline')
+const toolbar = document.querySelector('.toolbar__2');
+const topCross = document.querySelector('.top');
+const middleCross = document.querySelector('.middle');
+const bottomCross = document.querySelector('.bottom');
+const headline = document.querySelector('.headline');
 const works = document.querySelector('.works');
 const About = document.querySelector('.about-me');
-const form = document.querySelector('.form')
-const footer = document.querySelector('.footer')
+const form = document.querySelector('.form');
+const footer = document.querySelector('.footer');
+let trigger = document.getElementById('btn-trigger');
 
-function toggleNavShow(){
-    toolbar.classList.toggle('show');
-    topCross.classList.toggle('cross');
-    middleCross.classList.toggle('cross');
-    bottomCross.classList.toggle('disappear');
-    headline.classList.toggle('blur');
-    works.classList.toggle('blur');
-    About.classList.toggle('blur');
-    form.classList.toggle('blur');
-    footer.classList.toggle('blur');
+function toggleNavShow() {
+	toolbar.classList.toggle('show');
+	topCross.classList.toggle('cross');
+	middleCross.classList.toggle('cross');
+	bottomCross.classList.toggle('disappear');
+	headline.classList.toggle('blur');
+	works.classList.toggle('blur');
+	About.classList.toggle('blur');
+	form.classList.toggle('blur');
+	footer.classList.toggle('blur');
 }
 
-document.getElementById('btn-trigger').onclick = function() {
-    toggleNavShow();
-}
+trigger.addEventListener('click', function() {
+	toggleNavShow();
+});
 
-window.addEventListener('hashchange', function(){
-    toggleNavShow();
+window.addEventListener('hashchange', function() {
+	toggleNavShow();
 });
